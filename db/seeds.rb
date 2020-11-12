eb253f6e55740f6d61ceb719ea2cfeb9d49daff1
Maxime Lapraye <maxime.lapraye@gmail.com>
2020-11-12 17:03:16 -0500

    controller ok for create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+
+require "open-uri"
+require "faker"
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |i|
+  Ingredient.create!(name: i["strIngredient1"])
+end
+
+
+10.times do
+  cocktail = Cocktail.create!(
+    name: Faker::GreekPhilosophers.unique.name
+  )
+  3.times do
+    dose = Dose.new(description: ["2 onces", "just a finger", "50%"].sample)
+    dose.cocktail = cocktail
+    dose.ingredient = Ingredient.all.sample
+    dose.save!
+  end
+end
+
+# dose = Dose.new(description: "2 onces")
+# dose.cocktail = Cocktail.first
+# dose.ingredient = Ingredient.first
+# dose.save!
+
